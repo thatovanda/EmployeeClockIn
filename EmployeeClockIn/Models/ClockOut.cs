eb886b19be5e93758d072bbea89ed1c19e3f2be9
Thato Ludwe Churchill Vanda <21513842@dut4life.ac.za>
2019-10-15 11:18:53 +0200

    Clock out

---
+++ EmployeeClockIn/Models/ClockOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeClockIn.Models
+{
+    public class ClockOut
+    {
+        [Key]
+        public string ClockOutID { get; set; }
+
+        [Required]
+        [Display(Name = "Start Time")]
+        [DataType(DataType.Time)]
+        public TimeSpan EndTime { get; set; }
+
+        public int EmpId { get; set; }
+        public Employee Employee { get; set; }
+
+    }
+}
